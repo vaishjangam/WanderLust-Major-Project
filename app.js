af3826920968e048b6788b5ede80381bbccb488d
Vaishnavi Jangam <vaishnavijangam.vj@gmail.com>
2026-05-12 23:41:03 +0530

    Fixed flash and currUser locals

--- app.js
+++ app.js
@@ -74,7 +74,12 @@ const sessionOptions = {
 
 app.use(session(sessionOptions));
 app.use(flash());
-
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
+});
 app.use(passport.initialize());
 app.use(passport.session());
 
